4464709a6ef5c9dca3be67a94f8a14f4338505a1
charithakaluhendiwala <hmcnbkaluhendiwala@gmail.com>
2021-05-11 08:13:56 +0530

    final commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      {/* {navbar} */}
+      {/* {sideDraw} */}
+      {/* {backdrop} */}
+      {/* {homeScreen} */}
+      {/* {cartscreen} */}
+    </div>
+  );
+}
+
+export default App;
